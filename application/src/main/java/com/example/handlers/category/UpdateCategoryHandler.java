77877dd1441e76897a3e4901d3b6d8c02989f91a
Igor <igopood33@gmail.com>
2025-07-13 19:13:06 +0200

    fix: category update fix (changed name from path to body)

--- application/src/main/java/com/example/handlers/category/UpdateCategoryHandler.java
+++ application/src/main/java/com/example/handlers/category/UpdateCategoryHandler.java
@@ -3,6 +3,7 @@ package com.example.handlers.category;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.example.handlers.AbstractHandler;
+import com.example.model.request.CategoryRequest;
 import com.example.model.response.ResponseMessage;
 import com.example.service.contract.CategoryService;
 
@@ -16,7 +17,8 @@ public class UpdateCategoryHandler extends AbstractHandler<CategoryService> {
     protected HandlerResponse handleRequestLogic(APIGatewayProxyRequestEvent request, Context context) {
         int userId = authService.getUserId(request, context);
         int categoryId = Integer.parseInt(request.getPathParameters().get("id"));
-        String name = request.getPathParameters().get("name");
+        CategoryRequest categoryRequest = gson.fromJson(request.getBody(), CategoryRequest.class);
+        String name = categoryRequest.getName();
 
         service.update(categoryId, name, userId);
 
